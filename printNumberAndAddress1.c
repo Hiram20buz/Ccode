473e606d5c71025cdbc4cfa1735f8ec56a96ea55
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-08-14 21:46:59 -0700

    Create printNumberAndAddress1.c

---
+++ printNumberAndAddress1.c
@@ -0,0 +1,14 @@
+// C program to implement
+// the above approach
+#include <stdio.h>
+// Driver code
+
+int main()
+{
+    char abc[]="abc";
+    char xyz[]="xyz";
+
+    char *p=abc;
+    printf("%s,%s,%p/n",abc,xyz,p);
+    return 0;
+}
