a575418833e609e42977818b7bf781576d5fbbb3
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-08-14 21:42:36 -0700

    Create printNumbers.c

---
+++ printNumbers.c
@@ -0,0 +1,12 @@
+// C program to implement
+// the above approach
+#include <stdio.h>
+// Driver code
+
+int main()
+{
+    int a=21;
+    int b=17;
+    printf("a=%d, b=%d\n",a,b);
+    return 0;
+}
